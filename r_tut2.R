1b79c429f718caba3d2bd0360c19ec02bf7ed7a9
Róisin O'Donnell <55452431+roisinod@users.noreply.github.com>
2021-01-14 11:13:10 +0000

    Add files via upload

--- r_tut2.R
+++ r_tut2.R
@@ -10,3 +10,24 @@ Num = 10
 #assign type
 as.numeric(Num)
 is.numeric(Num)
+#operators
+#%d is decimals
+#addition
+sprintf("4 + 5 = %d", 4 + 5)
+#subtraction
+sprintf("4 - 5 = %d", 4 - 5)
+#multiplication
+sprintf("4 * 5 = %d", 4 * 5)
+#division
+sprintf("4 / 5 = %1.3f", 4 / 5)
+#get remainder from division using modulus
+sprintf("5 %% 4 = %d", 5 %% 4)
+#squared = ^ 
+sprintf("4 ^ 2 = %d", 4 ^ 2)
+#vectors
+numbers = c(3, 2, 0, 1, 8)
+numbers
+#using index - NB starts with 1 not 0
+numbers[1]
+#length
+length(numbers)
