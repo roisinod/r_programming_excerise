d53c3fd92a7aa7867ec45be6e99b6a00ac909bfe
Róisin O'Donnell <55452431+roisinod@users.noreply.github.com>
2021-01-14 13:44:39 +0000

    Add files via upload

---
+++ r_tut4_logical_op.R
@@ -0,0 +1,7 @@
+#logical operators
+#And operator
+cat("TRUE && FALSE = ", T && F, "/n")
+#Or operator
+cat("TRUE || FALSE = ", T || F, "/n")
+#not operator - returns the opposite of whatever is given
+cat("!TRUE = ", !T, "/n")
