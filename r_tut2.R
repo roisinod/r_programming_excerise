86284d6292bcaeb09be1cf34c7b67022b2a620cc
Róisin O'Donnell <55452431+roisinod@users.noreply.github.com>
2021-01-14 12:07:01 +0000

    Add files via upload

--- r_tut2.R
+++ r_tut2.R
@@ -31,3 +31,29 @@ numbers
 numbers[1]
 #length
 length(numbers)
+#get the last number
+numbers[length(numbers)]
+#get everything but the first index
+numbers[-1]
+#vector to get certain values by index
+numbers[c(1,2)]
+#another way without vector
+numbers[2:3]
+#replace value in a certain index
+numbers[5] = 1
+#sort
+sort(numbers, decreasing = T)
+#create a sequence of numbers
+sequence = 1:10
+#addition in sequence - creating a sequence from 1 to 20 that adds five each time
+plus3 = seq(from = 1, to = 20, by = 5)
+plus3
+#creating a sequence of even numbers
+evennumbers = seq(from = 2, by = 2, length.out = 10)
+evennumbers
+#find out if value is inside of a vector
+sprintf("4 in evennumbers %s", 4 %in% evennumbers)
+#repeating a value
+rep(x = 2, times = 5, each = 2)
+#more complex repeat pattern
+rep(x = c(1,2,3), times = 3, each = 2)
