8ea4cc14c88de1d8f4251bc79a09ecb195216631
Róisin O'Donnell <55452431+roisinod@users.noreply.github.com>
2021-01-15 14:04:49 +0000

    Add files via upload

--- r_tut9_looping.R
+++ r_tut9_looping.R
@@ -34,3 +34,21 @@ matrix2
 #filling rows before columns
 matrix3 = matrix(data=c(1,2,3,4), nrow = 2, ncol = 2, byrow=T)
 matrix3
+#get dimensions of matrix
+dim(matrix3)
+#get a value 
+matrix3[1,2]
+#get an entire row
+matrix3[1,]
+#get a whole column
+matrix3[,2]
+#combine vectors to make a matrix
+matrix4 = rbind(1:3, 4:6, 7:9)
+matrix4
+matrix4[2:3,]
+matrix4[-1,]
+#change values
+matrix4[1,1] = 0
+#change entire first row
+matrix4[1,] = c(10, 11, 12)
+matrix4
