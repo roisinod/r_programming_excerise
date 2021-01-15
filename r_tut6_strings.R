332f0b7485288714cd71f14a4ca4f430c2a3fe85
Róisin O'Donnell <55452431+roisinod@users.noreply.github.com>
2021-01-15 10:51:48 +0000

    Add files via upload

---
+++ r_tut6_strings.R
@@ -0,0 +1,9 @@
+#strings
+string1 = "This is a string"
+#finding the number of characters in a string
+nchar(string1)
+#comparing strings
+sprintf("Dog > Catt : %s", "Dog" > "Catt")
+sprintf("Dog == Cat : %s", "Dog" == "Cat")
+
+#combing
