236ece33d6434045c16eacc22b617c91f15e68f1
Róisin O'Donnell <55452431+roisinod@users.noreply.github.com>
2021-01-20 11:53:00 +0000

    Add files via upload

--- r_tut15_math_functions.R
+++ r_tut15_math_functions.R
@@ -1,3 +1,32 @@
 #math functions
 sqrt(x=100)
-log 
+log(x=4, base=2)
+exp(x = 2)
+sum(c(1,2,3))
+
+numbers = c(1,6,7,4)
+
+#find mean
+mean(numbers)
+#find median
+median(numbers)
+#min
+min(numbers)
+#max
+max(numbers)
+#range
+range(numbers)
+#round up 
+ceiling(4.5)
+#round down
+floor(4.5)
+#cumulative sum
+cumsum(c(1,2,3))
+#cumulative products
+cumprod(c(1,2,3))
+#cumulative max
+cummax(c(7:9, 4:6, 1:3))
+#cumulative min
+cummin(c(7:9, 4:6, 1:3))
+#generate number of coin flips
+sample(0:1, 10, replace = T)
